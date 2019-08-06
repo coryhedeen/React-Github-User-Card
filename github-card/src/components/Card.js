156eb3ba616f00c9e4442a9a1422d86b7b2f7e54
Cory Hedeen <hedeencory@gmail.com>
2019-08-06 16:40:40 -0400

    Revise Card to accomodate followers

--- github-card/src/components/Card.js
+++ github-card/src/components/Card.js
@@ -3,14 +3,13 @@ import React from 'react';
 const Card = props => {
   return (
     <div className="card">
-    <div className="imgContainer">
+
       <img src={props.card.avatar_url} alt="profile pic"/>
-    </div>
+    <div className="text">
       <h2>{props.card.name}</h2>
       <h3>{props.card.location}</h3>
-      <p>Public Repos: {props.card.public_repos}</p>
-      <p>Followers: {props.card.followers}</p>
-      <p>Following: {props.card.following}</p>
+      <h3>{props.card.login}</h3>
+    </div>
     </div>
   );
 }
